3e91b4170532e916378a8126400aa95b436678c9
DanNoble <danielwai999@gmail.com>
2023-05-03 13:11:42 -0400

    MongoDB API not connected with front end yet

--- index.js
+++ index.js
@@ -1,22 +1,89 @@
-// 1. Load your dependencies
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
-// 2. Instantiate an express instance
 const app = express();
-// 3. Define a port number to run your server on
-const PORT = 3000;
+const config = require('./config');
 
-// 4. Get the path to your public directory
+const PORT = config.PORT;
+
+// ---- Connect to mongodb here ----
+
+// read in mongoose library
+const mongoose = require('mongoose');
+// read in the URI to our MongoDB Atlas 
+const MONGODB_URI = config.MONGODB_URI;
+// Use mongoose to connect to our MongoDB Atlas server
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+
+// --- connect to your collection ---
+const todos = require('./models/todo');
+
+// Handle data in a nice way
+app.use(express.json());
 const publicURL = path.resolve(`${__dirname}/public`);
-// 5. Define the folder which will host your static files 
+
+// Set your static server
 app.use(express.static(publicURL));
 
-// 6. Define the route where your static HTML fill be found
-app.get("/", (req, res, next) => {
-  res.sendFile("index.html");
+// Set your static html file
+app.get("/", (req, res) => {
+  res.sendFile( path.resolve(__dirname + "/views/index.html"))
 });
 
-// 7. Start your webserver by listening to the port you defined
-app.listen(PORT, () => {
-  console.log(`see the magic at: http://localhost:${PORT}`)
+// ---- ADD YOUR API ENDPOINTS HERE ----
+// GET: "api/v1/todos"
+app.get("/api/v1/todos", async (req, res) => {
+  try{
+    const data = await todos.find();
+    res.json(data);
+  } catch(error){
+    console.error(error);
+    res.json(error);
+  }
 });
+
+// POST: "api/v1/todos"
+app.post("/api/v1/todos", async (req, res) => {
+  try{
+    const newData = {
+      todo: req.body.todo,
+      status: req.body.status
+    }
+    const data = await todos.create(newData);
+    res.json(data);
+  } catch(error){
+    console.error(error);
+    res.json(error);
+  }
+});
+
+// PUT: "api/v1/todos:id"
+app.put("/api/v1/todos/:id", async (req, res) => {
+  try{
+    const updatedData = {
+      todo: req.body.todo,
+      status: req.body.status
+    }
+    const data = await todos.findOneAndUpdate({_id: req.params.id}, updatedData, {new:true});
+    res.json(data);
+  } catch(error){
+    console.error(error);
+    res.json(error);
+  }
+});
+
+// DELETE: "api/v1/todos:id"
+app.delete('/api/v1/todos/:id', async (req, res) => {
+  try {
+  const deletedDocument = await treeDB.findOneAndDelete(req.params.id);
+  res.json({"message":"successfully removed item", "data": JSON.stringify(deletedDocument) });
+  } catch (error) {
+    res.json({ error: JSON.stringify(error) });
+  }
+});
+
+
+// Start listening
+app.listen(PORT, () => {
+  console.log(`see the magic: http://localhost:${PORT}`);
+})
