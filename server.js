e682369c95db88d9f23d1d0cfc64f2f4670e1a3f
DanNoble <danielwai999@gmail.com>
2023-05-03 00:35:15 -0400

    Node and MongoDB connection

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const uri = 'mongodb+srv://DanNoble:sEIszJowH3ELeIg8@recipecluster.geinzyi.mongodb.net/?retryWrites=true&w=majority'
+
+async function connect(){
+    try{
+        await mongoose.connect(uri);
+        console.log("Connected to MongoDB");
+    } catch (error){
+        console.error(error);
+    }
+}
+
+connect();
+
+app.listen(8000, () => {
+    console.log("Server started on port 8000")
+});
+
